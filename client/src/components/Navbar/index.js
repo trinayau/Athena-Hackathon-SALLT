2b537856344f6b8675449f6276d5852e92f50823
Trina <92634994+trinayau@users.noreply.github.com>
2022-10-22 20:56:36 +0100

    add netlify toml

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -71,7 +71,7 @@ const NavBar = () => {
             }}
             style={{color:"black"}}
           >
-            FullFill
+            HomeCompare
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
